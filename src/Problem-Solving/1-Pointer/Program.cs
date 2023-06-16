a9b597095e63b24d3f917d6ae7167ecb570355b5
sojib <mdsojibhosen444@gmail.com>
2023-06-16 10:47:29 +0600

    Probelem Addeed

---
+++ src/Problem-Solving/1-Pointer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+int[] nums = new int[] { 2,3,5,1,-3,-5,-6,2};
+
+void swap(int[] arr, int a, int b)
+{
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+int low = 0, high = nums.Length-1;
+
+for (int i = 0;i < nums.Length; i++)
+{
+    if (nums[i] < 0)
+    {
+        swap(nums,i,high--);
+        low++;
+    }
+
+}
+
+foreach(var item in nums)
+{
+    Console.WriteLine(item);
+}
---
+++ src/Problem-Solving/move all positive number to the right side/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] nums = { 2, -5, 7, 3 - 6 - 7, 2, 87, -3, -7, 2 ,-4,-8,-2,-6,-3};
+
+void swap(int[] arr,int a ,int b)
+{
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+int low = 0, high = nums.Length - 1;
+
+while (low < high)
+{
+    if (nums[low] > 0)
+    {
+        swap(nums, low, high--);
+    }
+    else
+    {
+        low++;
+    }
+}
+
+foreach(var item in nums)
+{
+    Console.WriteLine(item);
+}
