246088ca57ca9000936c0f0222d926725272b673
sojib <mdsojibhosen444@gmail.com>
2023-08-05 11:29:32 +0600

    commit added

---
+++ src/Problem-Solving/Find Odd Occurance in an array/Program.cs
@@ -0,0 +1,43 @@
+﻿
+int[] nums = { 2,1, 1, 2, 2, 3, 3, 4, 4, 2, 2 };
+
+int start = 0;
+int end = nums.Length - 1;
+
+int answer = 0;
+while(start < end)
+{
+    int mid = start + (end - start)/2;
+
+    if(start == end)
+    {
+        answer = nums[start];
+    }
+
+    if(mid%2 == 0)
+    {
+        if (nums[mid] == nums[mid+1])
+        {
+            start = mid + 2;
+        }
+        else
+        {
+            end = mid;
+        }
+    }
+    else
+    {
+        if (nums[mid] == nums[mid-1])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+}
+
+Console.WriteLine(nums[start]);
+
+
