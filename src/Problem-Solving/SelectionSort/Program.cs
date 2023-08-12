87dc7b52aad38f332e16984bc86540380cb5cbc1
sojib <mdsojibhosen444@gmail.com>
2023-08-12 23:42:16 +0600

    practice

---
+++ src/Problem-Solving/SelectionSort/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int[] arr = { 4, 2, 1, 0, 3, 5 };
+
+for(int i=0;i<arr.Length;i++)
+{
+    for(int j=0;j<arr.Length-1;j++)
+    {
+        if (arr[j] > arr[j+1])
+        {
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+
+foreach (var item in arr)
+{
+    Console.Write(item + " ");
+}
---
+++ src/Problem-Solving/Teat/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            string s = null;
+            Console.WriteLine(s[0]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An exception occurred: {ex.Message}");
+        }
+    }
+}
