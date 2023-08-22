2702f04b30936b1a4f6c040255b42a515aa0be2e
sojib <mdsojibhosen444@gmail.com>
2023-08-23 00:48:23 +0600

    cxx

---
+++ src/Problem-Solving/1047. Remove All Adjacent Duplicates In String/Program.cs
@@ -0,0 +1,88 @@
+﻿
+
+using System.Text;
+
+string sen = "azxxzy";
+
+//unique character
+StringBuilder stringBuilder = new StringBuilder();
+
+int[] arr = new int[27];
+
+for(int i = 0; i < sen.Length; i++)
+{
+    int ascii = sen[i]-96;
+    arr[ascii]++;
+}
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] == 1)
+    {
+        int ascii = i + 96;
+
+        char ch = (char)ascii;
+        stringBuilder.Append(ch);
+    }
+}
+
+Console.WriteLine(stringBuilder);
+
+//Remove all adjacent duplicate 
+StringBuilder removeDuplicate = new StringBuilder();
+
+for(int i = 0;i < sen.Length - 1;)
+{
+    if (sen[i] == sen[i+1])
+    {
+        i = i + 2;
+    }
+    else
+    {
+        removeDuplicate.Append(sen[i]);
+        i++;
+    }
+}
+
+Console.WriteLine(removeDuplicate);
+
+
+StringBuilder str = new();
+str.Append(sen);
+
+
+bool flag = true;
+
+while (flag)
+{
+    bool change = false;
+
+    for (int i = 0; i < str.Length - 1; i++)
+    {
+        if (str[i] == str[i + 1])
+        {
+            str.Remove(i, 2);
+            change = true;
+        }
+    }
+
+    if (!change)
+    {
+        flag = false;
+    }
+}
+
+
+
+
+Console.WriteLine(str);
+
+HashSet<char> chars = new HashSet<char>();
+
+chars.Add('a');
+chars.Add('a');
+
+foreach (var item in chars)
+{
+    Console.WriteLine(item);
+}
---
+++ src/Problem-Solving/532. K-diff Pairs in an Array/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] arr = { 3, 1, 4, 1, 5 };
+
+Array.Sort(arr);
+
+int k = 2;
+
+for(int i = 0; i < arr.Length-1; i++)
+{
+    if (arr[i] != arr[i+1])
+    {
+        int d = Math.Abs(arr[i] - arr[i + 1]);
+
+        if(d==k)
+        {
+
+        }
+    }
+}
+
