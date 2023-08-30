bbcbeee26eef4902b7d5146a69a9e0f315bda258
sojib <mdsojibhosen444@gmail.com>
2023-08-31 01:22:35 +0600

    practice

--- src/Problem-Solving/1910. Remove All Occurrences of a Substring/Program.cs
+++ src/Problem-Solving/1910. Remove All Occurrences of a Substring/Program.cs
@@ -2,29 +2,19 @@
 
 //Approach bruit force approach
 
-string name = "abbbabcaabcghabc";
-string part = "abc";
+string name = "eemckxmckx";
+string part = "emckx";
 
 int partLength = part.Length;
-char partFirstCharacter = part[0];
 
-bool flag = true;
-
-while (flag)
+while(name.IndexOf(part) > 0)
 {
-    for (int i = 0; i < name.Length; i++)
-    {
-        if (name[i] == partFirstCharacter)
-        {
-            string subStringFromName = name.Substring(i, partLength);
-
-            if (subStringFromName == part)
-            {
-                name.Remove(i, partLength);
-            }
-        }
-    }
+    name = name.Remove(name.IndexOf(part), partLength);
 }
+
+
+ Console.WriteLine(name);
+
 
 
 
