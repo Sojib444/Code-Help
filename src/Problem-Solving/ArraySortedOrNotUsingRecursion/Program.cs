5c5be749727bd1d2c1886b756d249e7907b1f8c1
sojib <mdsojibhosen444@gmail.com>
2023-09-28 23:27:33 +0600

    Practice

---
+++ src/Problem-Solving/ArraySortedOrNotUsingRecursion/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int[] arr = { 1, 10, 3, 4, 5, 6 };
+int i = 0;
+
+bool Sort(int[] arr, int i)
+{
+    if(i >= arr.Length)
+    {
+        return true;
+    }
+
+    if (i < arr.Length-1 && arr[i] > arr[i+1])
+    {
+        return false;
+    }
+
+    bool ans = Sort(arr, ++i);
+
+    return ans;
+}
+
+bool d = Sort(arr, i);
+
+Console.WriteLine(d);
---
+++ src/Problem-Solving/FindMaximuminAnArrayintoRecursion/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int[] arr = { 1, 300, 4 ,10};
+
+int max = int.MinValue;
+int i = 0;
+
+void findMax(int[] arr, int i,ref int max)
+{
+    if(i == arr.Length)
+    {
+        return ;
+    }
+
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+
+    findMax(arr, ++i,ref max);
+}
+ 
+findMax(arr, i, ref max);
+
+Console.WriteLine(max);
