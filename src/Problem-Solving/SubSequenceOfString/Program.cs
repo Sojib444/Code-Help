4ff9e23dd9234bc26baa80c1f0f160a052704332
sojib <mdsojibhosen444@gmail.com>
2023-09-30 22:55:34 +0600

    Practice

---
+++ src/Problem-Solving/SubSequenceOfString/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string s = "abc";
+int i = 0;
+void include(string s,int i,string output)
+{
+    if(i >= s.Length)
+    {
+        Console.WriteLine(output);
+        return;
+    }
+
+    //exclude
+    include(s, i+1, output);
+
+    //include
+    output = output + s[i];
+    include(s, i+1, output);
+}
+
+include(s, i, "");
---
+++ src/Problem-Solving/bainarySearchusingRecusion/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] d = { 1, 2, 3, 4, 5, 6 };
+
+int s = 0;
+int e = d.Length - 1;
+int target = 20;
+
+bool isPresent(ref int[]  arr, int s, int e, int target)
+{
+    if(s > e)
+    {
+        return false;
+    }
+
+    int mid = s + (e - s) / 2;
+
+    if (arr[mid] == target)
+    {
+        return true;
+    }
+    if (arr[mid] > target)
+    {
+        return isPresent(ref arr, s, mid - 1,target);
+    }
+    else
+        return isPresent(ref arr, mid + 1, e,target);
+}
+
+Console.WriteLine(isPresent(ref d, s, e, target));
