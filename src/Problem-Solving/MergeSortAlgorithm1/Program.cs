496321eb76a0ca76c87102504f04015c02597fd0
sojib <mdsojibhosen444@gmail.com>
2023-10-31 13:29:25 +0600

    Practice

--- src/Problem-Solving/MergeSortAlgorithm1/Program.cs
+++ src/Problem-Solving/MergeSortAlgorithm1/Program.cs
@@ -4,31 +4,6 @@ int[] nums = { 2, 1, 4, 15, 3,0 };
 int start = 0;
 int end = nums.Length - 1;
 
-void MergeSort(int[] arr, int start, int end)
-{
-    if(start == end)
-    {
-        return;
-    }
-
-    int mid = start + (end - start) / 2;
-
-    //left Side part of this array
-    MergeSort(arr, start, mid);
-
-    //right side part of this array
-    MergeSort(arr, mid + 1, end);
-
-    Merge(arr,start,mid,end);
-}
-
-MergeSort(nums, start, end);
-
-foreach (var item in nums)
-{
-    Console.Write(item + " ");
-}
-
 void Merge(int[] arr, int start, int mid, int end)
 {
     int l = mid - start + 1;
@@ -39,13 +14,13 @@ void Merge(int[] arr, int start, int mid, int end)
 
     int k = start;
 
-    for(int i = 0; i<l;i++)
+    for (int i = 0; i < l; i++)
     {
         left[i] = arr[k];
         k++;
     }
 
-    k = mid+1;
+    k = mid + 1;
 
     for (int j = 0; j < r; j++)
     {
@@ -71,13 +46,39 @@ void Merge(int[] arr, int start, int mid, int end)
         }
     }
 
-    while(a<l)
+    while (a < l)
     {
-        arr[arrIndedx++] = left[a++]; 
+        arr[arrIndedx++] = left[a++];
     }
 
-    while(b<r)
+    while (b < r)
     {
         arr[arrIndedx++] = right[b++];
     }
 }
+
+void MergeSort(int[] arr, int start, int end)
+{
+    if(start == end)
+    {
+        return;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    //left Side part of this array
+    MergeSort(arr, start, mid);
+
+    //right side part of this array
+    MergeSort(arr, mid + 1, end);
+
+    Merge(arr,start,mid,end);
+}
+
+MergeSort(nums, start, end);
+
+foreach (var item in nums)
+{
+    Console.Write(item + " ");
+}
+
---
+++ src/Problem-Solving/QuickSort/Program.cs
@@ -0,0 +1,79 @@
+﻿
+int[] arr = { 100,99,2, 4, 1, 3 };
+
+int s = 0, e = arr.Length - 1;
+
+void QuickSort(int[] arr, int s, int e)
+{
+    if(s >= e)
+    {
+        return;
+    }
+    int pivotIndex = PivotIndex(arr,s,e);
+
+    //left side 
+    QuickSort(arr, s, pivotIndex - 1);
+
+    //right side
+    QuickSort(arr, pivotIndex + 1, e);
+}
+
+void Swap(int[] arr, int a, int b)
+{
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+
+}
+
+int PivotIndex(int[] arr, int s, int e)
+{
+
+    int count = 0;
+
+    for(int i = s+1; i <= e; i++)
+    {
+        if (arr[i] < arr[s])
+        {
+            count++;
+        }
+    }
+
+    int rightIndex = s + count;
+
+    Swap(arr, s, rightIndex);
+
+    int a = s;
+    int b = e;
+
+    while(a < rightIndex && b > rightIndex)
+    {
+        while (arr[a] < arr[rightIndex])
+        {
+            a++;
+        }
+
+        while (arr[b] > arr[rightIndex])
+        {
+            b--;
+        }
+
+        if(a < rightIndex && b > rightIndex)
+        {
+            Swap(arr, a, b);
+        }
+    }
+
+    return rightIndex;
+}
+
+QuickSort(arr, s, e);
+
+foreach(var item in arr)
+{
+    Console.Write(item + " ");
+}
+
+//time complexity in avarage case O(nlog) // in "merge sort" avarage and worst case time complexity is O(nlogn) 
+//time complexity in worst case O(n^2) //
+//space complexity O(n)
