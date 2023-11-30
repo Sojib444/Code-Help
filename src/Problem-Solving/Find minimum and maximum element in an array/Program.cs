305971233a0722c0a81903a01089113c3d8cfd71
sojib <mdsojibhosen444@gmail.com>
2023-11-30 21:09:37 +0600

    Practice

---
+++ src/Problem-Solving/Find minimum and maximum element in an array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+int[] arr = [2, 4, 5, 8, 3];
+
+int max = int.MinValue;
+int min = int.MaxValue;
+
+foreach(var item in arr)
+{
+    if(item  > max)
+    {
+        max = item;
+    }
+
+    if(item < min)
+    {
+        min = item;
+    }
+}
+
+Console.WriteLine(max);
+Console.WriteLine(min);
+
+//Space complexity O(1);
+//Time complexity O(n);
+
+/* we can use Sort algorithm which takes O(nlogn) time complexity*/
---
+++ src/Problem-Solving/Password Hassing/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+void HasPassword(string password)
+{
+    byte[] input = System.Text.Encoding.UTF8.GetBytes(password);
+
+    long key = 116421312659916;
+
+    foreach (byte b in input)
+    {
+        key = (key << 2)+ b;
+    }
+
+    string hasingResult = key.ToString("X");
+
+}
+
+void RevertHash(string password)
+{
+    byte[] input = System.Text.Encoding.UTF8.GetBytes(password);
+
+    long key = 116421312659916;
+
+    foreach (byte b in input)
+    {
+        key = (key >> 2) - b;
+    }
+
+    Console.WriteLine(key.ToString());
+}
+
+HasPassword("sojib");
