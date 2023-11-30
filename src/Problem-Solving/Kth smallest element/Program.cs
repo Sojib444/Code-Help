983fff591970f9c2c37691b070a64c74b8369105
sojib <mdsojibhosen444@gmail.com>
2023-11-30 22:25:32 +0600

    Added LinkList

---
+++ src/Problem-Solving/Kth smallest element/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Given an array arr[] and an integer K where K is smaller than size of array,
+ the task is to find the Kth smallest element in the given array. 
+ It is given that all array elements are distinct.*/
+
+int[] arr = { 7, 10, 4, 3, 20, 15 };
+
+Array.Sort(arr);
+
+Console.WriteLine(arr[3-1]);
+
+
+//Lets apply Quick Sort ALgorithm
---
+++ src/Problem-Solving/LinkedInList/Program.cs
@@ -0,0 +1,40 @@
+﻿
+NodeList head = new(10);
+NodeList list1 = new(20);
+NodeList list2 = new(30);
+NodeList list3 = new(40);
+
+head.next = list1;
+list1.next = list2;
+list2.next = list3;
+list3.next = null;
+
+NodeList temp = head;
+
+while(temp.next != null)
+{
+    Console.WriteLine(temp.data);
+    temp = temp.next;
+}
+
+public class NodeList
+{
+    public NodeList()
+    {
+        data = 0;
+        next = null;
+    }
+
+    public NodeList(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+    public int data { get; set; }
+    public NodeList? next { get; set; }
+}
+
+
+
+
+
